8674a801a81da001c2e89e7ce429725bc59a35d6
Yurii <stepanuk.ura2001@gmail.com>
2024-04-26 21:43:01 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+export const App = () => {
+  return (
+    <>
+      <p className="text-3xl">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam
+        dignissimos aperiam cumque reiciendis fugit voluptates dolores.
+        Repellat, est tempora optio impedit adipisci suscipit consequatur
+        obcaecati, cum eligendi blanditiis veniam doloribus!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam
+        dignissimos aperiam cumque reiciendis fugit voluptates dolores.
+        Repellat, est tempora optio impedit adipisci suscipit consequatur
+        obcaecati, cum eligendi blanditiis veniam doloribus!
+      </p>
+    </>
+  );
+};
